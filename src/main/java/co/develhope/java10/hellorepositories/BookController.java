2ba742ee80f7d74c8bc0daf7c1ec6f868ba1d256
aegroto <aegroto@protonmail.com>
2023-05-02 15:32:38 +0200

    get one book with optional in implementation

--- src/main/java/co/develhope/java10/hellorepositories/BookController.java
+++ src/main/java/co/develhope/java10/hellorepositories/BookController.java
@@ -1,9 +1,12 @@
 package co.develhope.java10.hellorepositories;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +21,16 @@ public class BookController {
     @GetMapping
     public Collection<Book> getAll() {
         return bookRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> get(@PathVariable int id) {
+        Optional<Book> result = bookRepository.findById(id);
+        if(result.isPresent()) {
+            return ResponseEntity.ok().body(result.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @PostMapping
