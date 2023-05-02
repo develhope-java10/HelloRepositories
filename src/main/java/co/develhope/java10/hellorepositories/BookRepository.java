be41983267a9e8e3999a88723fe077e7f9d43dad
aegroto <aegroto@protonmail.com>
2023-05-02 15:19:59 +0200

    full crud repository

--- src/main/java/co/develhope/java10/hellorepositories/BookRepository.java
+++ src/main/java/co/develhope/java10/hellorepositories/BookRepository.java
@@ -1,7 +1,11 @@
 package co.develhope.java10.hellorepositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Collection;
 
-public interface BookRepository extends JpaRepository<Book, Integer> {
-    
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface BookRepository extends CrudRepository<Book, Integer> {
+    public Collection<Book> findByTitleContaining(String title);
 }
